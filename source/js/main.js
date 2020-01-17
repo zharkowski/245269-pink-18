015f8e56ff24f7a69769b9d50a015716dd33b458
zharkowski <maxim199919@rambler.ru>
2020-01-17 18:17:29 +0300

    добавляет рабочее меню на js и декоративные элементы, фиксы

---
+++ source/js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else if (navMain.classList.contains('main-nav--opened')) {
+      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+    }
+  })
+})();
